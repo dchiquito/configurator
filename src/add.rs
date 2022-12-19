9194509aebfbe25a7ae051f2f24907b04f9f5772
Daniel Chiquito <daniel.chiquito@gmail.com>
2022-12-18 19:16:55 -0500

    Ensure repo directory exists before add

--- src/add.rs
+++ src/add.rs
@@ -6,6 +6,7 @@ pub fn add(ctx: &Context, file: &PathBuf) -> Result<(), Error> {
     let system_file = std::fs::canonicalize(file)?;
     let repo_file = ctx.absolute_to_configurator_path(&system_file);
     if ctx.are_files_different(&system_file, &repo_file) {
+        std::fs::create_dir_all(&repo_file.parent().unwrap())?;
         std::fs::copy(&system_file, &repo_file)?;
         println!("Added {}", repo_file.display());
     } else {
